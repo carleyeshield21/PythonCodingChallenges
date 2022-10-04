7186e3c083bd100b317c37efb480cf374135dd27
Carl Yamzon <92280593+carleyeshield21@users.noreply.github.com>
2022-10-04 14:55:00 +0800

    Update RadioDataSystem.py

--- RadioDataSystem.py
+++ RadioDataSystem.py
@@ -1,8 +1,8 @@
+# Here is the link to this Python coding challenge
 # https://www.codewars.com/kata/5ba58763924c945f950000a5/train/python
 str_input = True
 arr = ['']
 while str_input:
-    # input_string = arr.append(input('Type any string or endstr to end\n'))
     input_string = input('Type any string or "endstr" to end\n')
     if input_string != 'endstr':
         counter = 0
@@ -14,11 +14,13 @@ while str_input:
                 if counter == len(arr):
                     arr.append(input_string)
         # print(f'counter = {counter}')
+        # del arr[0]
         print(arr)
+        print(' '.join(arr))
     else:
         str_input = False
-print(arr)
 del arr[0]
+# arr.pop(0)
 print(arr)
 print(' '.join(arr))
 
