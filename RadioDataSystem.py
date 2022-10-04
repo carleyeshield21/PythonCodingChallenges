0a9154306ce7e0f4065f7558158e9e77d7bd57d5
Carl Yamzon <92280593+carleyeshield21@users.noreply.github.com>
2022-10-04 14:59:42 +0800

    Update RadioDataSystem.py

--- RadioDataSystem.py
+++ RadioDataSystem.py
@@ -1,28 +1,30 @@
 # Here is the link to this Python coding challenge
 # https://www.codewars.com/kata/5ba58763924c945f950000a5/train/python
-str_input = True
-arr = ['']
-while str_input:
-    input_string = input('Type any string or "endstr" to end\n')
-    if input_string != 'endstr':
-        counter = 0
-        for n in range(len(arr)):
-            # print(arr[n], input_string)
-            # print(arr[n] == input_string)
-            if arr[n] != input_string:
-                counter += 1
-                if counter == len(arr):
-                    arr.append(input_string)
-        # print(f'counter = {counter}')
-        # del arr[0]
-        print(arr)
-        print(' '.join(arr))
-    else:
-        str_input = False
-del arr[0]
-# arr.pop(0)
-print(arr)
-print(' '.join(arr))
+def RadioDataSystem():
+    str_input = True
+    arr = ['']
+    while str_input:
+        input_string = input('Type any string or "endstr" to end\n')
+        if input_string != 'endstr':
+            counter = 0
+            for n in range(len(arr)):
+                # print(arr[n], input_string)
+                # print(arr[n] == input_string)
+                if arr[n] != input_string:
+                    counter += 1
+                    if counter == len(arr):
+                        arr.append(input_string)
+            # print(f'counter = {counter}')
+            # del arr[0]
+            print(arr)
+            print(' '.join(arr))
+        else:
+            str_input = False
+    del arr[0]
+    # arr.pop(0)
+    print(arr)
+    print(' '.join(arr))
+RadioDataSystem()
 
 # Here set an array or list and has a single space as the initial value. The purpose of this is to have a basis
 # when we are trying to make a comparison in the if statement "if arr[n] != input_string:". We increment the counter
